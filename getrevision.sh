dce960758c461e5f0033db03ddd5e2cdf1407183
Juha Viitanen <juha.viitanen@exertus.fi>
2023-12-22 13:56:46 +0200

    Master -> main

--- getrevision.sh
+++ getrevision.sh
@@ -3,7 +3,7 @@ TAG=$(git describe --dirty --always --tags)
 SHA=$(git log -1 --pretty=format:"%h" ./ | cut -b -4)
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-if [ "${BRANCH}" = "master" ]; then
+if [ "${BRANCH}" = "main" ]; then
     echo ${TAG}
 else
     echo ${TAG}_${BRANCH}
