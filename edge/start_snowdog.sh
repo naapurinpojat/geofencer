a9657c15789baba448b39d5ca3886861223aadb6
Matti Wenell <matti.wenell@wapice.com>
2023-12-27 09:24:55 +0200

    Improve log files persistence. Also store process ID to file so it can be examined easily

--- edge/start_snowdog.sh
+++ edge/start_snowdog.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
+CURRENTDATE=`date +"%Y-%m-%d-%T"`
 filename="/tmp/connok"
 inotifywait -e create "$(dirname "$filename")" && [ -e "$filename" ] && echo "Connection OK!"
 
 curl http://juhaviitanen.com/snowdog/map.geojson > /home/snowdog/map.geojson
-nohup python /home/snowdog/snowdog.py > snowdog.log &
-ps|grep "python /home/snowdog/snowdog.py"
+nohup python /home/snowdog/snowdog.py > "snowdog-${CURRENTDATE}.log" &
+ps|grep "python /home/snowdog/snowdog.py" > "snowdog-thread.info"
