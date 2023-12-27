0f780a83e66594ee02b684fc53dadd16bce57ba9
Matti Wenell <matti.wenell@wapice.com>
2023-12-27 11:04:34 +0200

    Lint redis_client

--- edge/redis_client.py
+++ edge/redis_client.py
@@ -1,9 +1,18 @@
+"""Module providing Redis connection handlers"""
+from time import sleep
 import datetime
 import redis
-from time import sleep
 
 class RedisClient:
-    def __init__(self, host, port, topic, consumer_group, consumer_name, logger=None):
+    """Redis client to handle Redis read/write"""
+    # pylint: disable=too-many-arguments
+    def __init__(self,
+                 host,
+                 port,
+                 topic,
+                 consumer_group,
+                 consumer_name,
+                 logger=None):
         self.logger = logger
         self.host = host
         self.port = port
@@ -15,9 +24,8 @@ class RedisClient:
 
         try:
             self.client.xgroup_create(self.topic, self.consumer_group, id=0, mkstream=True)
-        except:
+        except redis.ResponseError:
             self.logger.debug("Consumer group most likely already existing")
-            pass
 
         self.logger.debug("Redis client initialised")
         start_time = datetime.datetime.now()
@@ -32,25 +40,31 @@ class RedisClient:
 
             if time_diff.total_seconds() >= 60:
                 logger.critical("Couldn't establish connection to redis")
-                self.shutdown_event.set()
-                break         
+                return
 
         self.logger.debug("Redis loaded")
 
     def read_messages(self, initial=False):
+        """Read messages from stream that is part of topic"""
+        _ = (initial) # use this later
         count = 1
 
         messages_ret = []
-        messages = self.client.xreadgroup(self.consumer_group, self.consumer_name, {self.topic: '>'}, count=count)
+        messages = self.client.xreadgroup(self.consumer_group,
+                                          self.consumer_name,
+                                          {self.topic: '>'},
+                                          count=count)
         for mes in messages:
             for mes_id, mes_data in mes[1]:
                 messages_ret.append(mes_data)
                 self.client.xack(mes[0], self.consumer_group, mes_id)
 
         return messages_ret
-    
+
     def add_message(self, message):
+        """Add message to redis stream"""
         self.client.xadd(self.topic, message)
 
     def close(self):
+        """Close redis client connection"""
         self.client.close()
