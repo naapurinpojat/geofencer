ab6a3a8c6959d4a6a11d4ac4638ea68669751bdd
Matti Wenell <matti.wenell@wapice.com>
2023-12-30 13:24:31 +0200

    Add secrets to use with docker env

---
+++ secrets_docker.py
@@ -0,0 +1,11 @@
+"""Hold secrets used in docker"""
+MY_DEVICE = "snowdog_1"
+MY_PWD="test_password_1234"
+MY_TENANT="random"
+HTTP_ADAPTER_IP="snowdog-mqtt-broker"
+MQTT_PORT=1883
+CERT_FILE=""
+TELEMETRY_ID="gpslatlonalt"
+
+API_KEY="0028b076-ca97-44c5-9603-bdfc38e2718e"
+WEB_API="http://apache-container:80/snowdog/api.php"
