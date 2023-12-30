de7369f1cbe058333e51e91d7d3cee07bc10137a
Matti Wenell <matti.wenell@wapice.com>
2023-12-30 13:37:21 +0200

    Use local web server to download map.geojson

--- edge/start_snowdog_docker.sh
+++ edge/start_snowdog_docker.sh
@@ -6,7 +6,7 @@ nohup socat pty,raw,echo=0,link=/dev/EG27.NMEA pty,raw,echo=0,link=/dev/EG25.NME
 nohup redis-server --bind 0.0.0.0 &
 . /home/snowdog/venv/bin/activate
 
-curl http://juhaviitanen.com/snowdog/map.geojson > /home/snowdog/map.geojson
+curl http://apache-container:80/snowdog/map.geojson > /home/snowdog/map.geojson
 nohup python /home/snowdog/nmea_testdata/emulator.py "/home/snowdog/nmea_testdata/snowdog-1.gpx" &
 python /home/snowdog/snowdog.py
 #ps|grep "python /home/snowdog/snowdog.py" > "snowdog-thread.info"
