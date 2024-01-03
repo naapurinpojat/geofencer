9c0169d91a7b534b68e332118d67615464e42a71
Matti Wenell <matti.wenell@wapice.com>
2024-01-03 09:09:42 +0200

    Use same python version in docker that is used in real snowdog (3.9)
    * Also do not use venv as it is not used in real HW neither
    * Install pip requirements in boot

--- edge/start_snowdog.sh
+++ edge/start_snowdog.sh
@@ -3,6 +3,8 @@ CURRENTDATE=`date +"%Y-%m-%d-%T"`
 filename="/tmp/connok"
 inotifywait -e create "$(dirname "$filename")" && [ -e "$filename" ] && echo "Connection OK!"
 
+pip install -r /home/snowdog/requirements.txt
+
 curl http://juhaviitanen.com/snowdog/map.geojson > /home/snowdog/map.geojson
 nohup python /home/snowdog/snowdog.py > "snowdog-${CURRENTDATE}.log" &
 ps|grep "python /home/snowdog/snowdog.py" > "snowdog-thread.info"
--- edge/start_snowdog_docker.sh
+++ edge/start_snowdog_docker.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 CURRENTDATE=`date +"%Y-%m-%d-%T"`
 
+pip install -r /home/snowdog/requirements.txt
+
 nohup socat pty,raw,echo=0,link=/dev/EG27.NMEA pty,raw,echo=0,link=/dev/EG25.NMEA > /dev/null &
 
 nohup redis-server --bind 0.0.0.0 &
-. /home/snowdog/venv/bin/activate
 
 curl http://apache-container:80/snowdog/map.geojson > /home/snowdog/map.geojson
 nohup python /home/snowdog/nmea_testdata/emulator.py "/home/snowdog/nmea_testdata/snowdog-1.gpx" &
@@ -12,4 +13,3 @@ nohup python /home/snowdog/nmea_testdata/emulator.py "/home/snowdog/nmea_testdat
 #nohup python /home/snowdog/nmea_testdata/emulator.py "/home/snowdog/nmea_testdata/snowdog-1.gpx" delay_test &
 
 python /home/snowdog/snowdog.py
-#ps|grep "python /home/snowdog/snowdog.py" > "snowdog-thread.info"
